29e6f2856ef9e360191ebe92db6c45eca809d635
振震 <865702942@qq.com>
2016-11-22 14:36:16 +0800

    makeXStyleElements function add

--- src/style.js
+++ src/style.js
@@ -1,3 +1,5 @@
+import { Xfont, Xfill, XpatternFill, Xborder, Xxf, Xalignment } from './xmlStyle';
+
 export class Style {
   applyBorder = false;
   applyFill = false;
@@ -9,10 +11,54 @@ export class Style {
     this.border = new Border({});
     this.fill = new Fill({});
     this.font = new Font({});
-    this.alignment = new Alignment({});
+    this.align = new Alignment({});
   }
   makeXStyleElements () {
+    const xFont = new Xfont({
+      sz: this.font.size,
+      name: this.font.name,
+      family: this.font.family,
+      charset: this.font.charset,
+      color: this.font.color,
+      b: this.font.bold,
+      i: this.font.italic,
+      u: this.font.underline
+    });
+    const xFill = new Xfill({
+      patternFill: new XpatternFill({
+        patternType: this.fill.patternType,
+        fgColor: this.fill.fgColor,
+        bgColor: this.fill.bgColor
+      })
+    });
+    const xBorder = new Xborder({
+      left: { style: this.border.left, color: this.border.leftColor },
+      right: { style: this.border.right, color: this.border.rightColor },
+      top: { style: this.border.top, color: this.border.topColor },
+      bottom: { style: this.border.bottom, color: this.border.bottomColor }
+    });
+    const xXf = new Xxf({
+      numFmtId: 0,
+      applyBorder: this.applyBorder,
+      applyFill: this.applyFill,
+      applyFont: this.applyFont,
+      applyAlignment: this.applyAlignment
+    });
 
+    xXf.alignment = new Xalignment({
+      horizontal: this.align.h,
+      indent: this.align.indent,
+      shrinkToFit: this.align.shrinkToFit,
+      textRotation: this.align.textRotation,
+      vertical: this.align.v,
+      wrapText: this.align.wrapText
+    });
+
+    if (this.namedStyleIndex !== null) {
+      xXf.xfId = this.namedStyleIndex;
+    }
+
+    return { xFont, xFill, xBorder, xXf };
   }
 }
 
@@ -53,10 +99,10 @@ export class Font {
 }
 
 export class Alignment {
-  indent =0;
-  shrinkToFit =false;
-  textRotation =0;
-  wrapText =false;
+  indent = 0;
+  shrinkToFit = false;
+  textRotation = 0;
+  wrapText = false;
 
   constructor ({ h = 'general', v = 'bottom' }) {
     this.h = h;
