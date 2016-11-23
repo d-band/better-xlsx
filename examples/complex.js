4f22556675a097c2584d8caee46d2cc2a074e547
振震 <865702942@qq.com>
2016-11-23 23:42:49 +0800

    lint fixed

--- examples/complex.js
+++ examples/complex.js
@@ -4,50 +4,115 @@ const xlsx = require('../lib').default;
 const file = new xlsx.File();
 const sheet = file.addSheet('Sheet1');
 const data = [
-  ['Auto', 200, 90],
-  ['Entertainment', 200, 32],
-  ['Food', 350, 205.75],
-  ['Home', 300, 250],
-  ['Medical', 100, 35],
-  ['Personal Items', 300, 80],
-  ['Travel', 500, 350],
-  ['Utilities', 200, 100],
-  ['Other', 50, 60]
+  ['Auto', 200, 90, 'B2-C2'],
+  ['Entertainment', 200, 32, 'B3-C3'],
+  ['Food', 350, 205.75, 'B4-C4'],
+  ['Home', 300, 250, 'B5-C5'],
+  ['Medical', 100, 35, 'B6-C6'],
+  ['Personal Items', 300, 80, 'B7-C7'],
+  ['Travel', 500, 350, 'B8-C8'],
+  ['Utilities', 200, 100, 'B9-C9'],
+  ['Other', 50, 60, 'B10-C10']
 ];
 
-for (let line of data) {
+function border(cell, top, left, bottom, right) {
+  const light = 'ffded9d4';
+  const dark = 'ff7e6a54';
+  cell.style.border.top = 'thin';
+  cell.style.border.topColor = top ? dark : light;
+  cell.style.border.left = 'thin';
+  cell.style.border.leftColor = left ? dark : light;
+  cell.style.border.bottom = 'thin';
+  cell.style.border.bottomColor = bottom ? dark : light;
+  cell.style.border.right = 'thin';
+  cell.style.border.rightColor = right ? dark : light;
+}
+
+function fill(cell, type) {
+  type = type || 0;
+  const colors = ['ffffffff', 'ffa2917d', 'ffe4e2de', 'fffff8df', 'fff1eeec'];
+  // 1: header, 2: first col, 3: second col, 4: gray, 0: white
+  cell.style.fill.patternType = 'solid';
+  cell.style.fill.fgColor = colors[type];
+  cell.style.fill.bgColor = 'ffffffff';
+}
+
+const header = sheet.addRow();
+header.setHeightCM(0.8);
+const headers = ['Category', 'Budget', 'Actual', 'Difference'];
+for (let i = 0; i < headers.length; i++) {
+  const hc = header.addCell();
+  hc.value = headers[i];
+  hc.style.align.v = 'center';
+  if (i > 0) hc.style.align.h = 'right';
+  hc.style.font.color = 'ffffffff';
+  border(hc, 0, 0, 1, 0);
+  fill(hc, 1);
+}
+
+const len = data.length;
+for (let i = 0; i < len; i++) {
+  const line = data[i];
   const row = sheet.addRow();
   row.setHeightCM(0.8);
-
+  // Col 1
   const cell1 = row.addCell();
   cell1.value = line[0];
-  cell1.style.border.left = 'thin';
-  cell1.style.border.leftColor = 'ffded9d4';
-  cell1.style.border.bottom = 'thin';
-  cell1.style.border.bottomColor = 'ffded9d4';
-  cell1.style.border.top = 'thin';
-  cell1.style.border.topColor = 'ff7e6a54';
-  cell1.style.border.right = 'thin';
-  cell1.style.border.rightColor = 'ff7e6a54';
-  cell1.style.fill.patternType='solid';
-  cell1.style.fill.fgColor='ffa2917d';
-  cell1.style.fill.bgColor='ffffffff';
-
+  cell1.style.align.v = 'center';
+  if (i === 0) {
+    border(cell1, 1, 0, 0, 1);
+  } else if (i === len - 1) {
+    border(cell1, 0, 0, 1, 1);
+  } else {
+    border(cell1, 0, 0, 0, 1);
+  }
+  fill(cell1, 2);
+  // Col 2
   const cell2 = row.addCell();
   cell2.value = line[1];
-  cell2.numFmt = 8;
+  cell2.numFmt = '$#,##0.00';
   cell2.cellType = 'TypeNumeric';
-  cell2.style.border.right = 'thin';
-  cell2.style.border.rightColor = 'ffded9d4';
-  cell2.style.border.bottom = 'thin';
-  cell2.style.border.bottomColor = 'ffded9d4';
-  cell2.style.border.top = 'thin';
-  cell2.style.border.topColor = 'ff7e6a54';
-  cell2.style.border.left = 'thin';
-  cell2.style.border.leftColor = 'ff7e6a54';
-  cell2.style.fill.patternType='solid';
-  cell2.style.fill.fgColor='fffff8df';
-  cell2.style.fill.bgColor='ffffffff';
+  cell2.style.align.v = 'center';
+  if (i === 0) {
+    border(cell2, 1, 1, 0, 0);
+  } else if (i === len - 1) {
+    border(cell2, 0, 1, 1, 0);
+  } else {
+    border(cell2, 0, 1, 0, 0);
+  }
+  fill(cell2, 3);
+  // Col 3
+  const cell3 = row.addCell();
+  cell3.value = line[2];
+  cell3.numFmt = '$#,##0.00';
+  cell3.cellType = 'TypeNumeric';
+  cell3.style.align.v = 'center';
+  if (i === 0) {
+    border(cell3, 1, 0, 0, 0);
+  } else if (i === len - 1) {
+    border(cell3, 0, 0, 1, 0);
+  } else {
+    border(cell3, 0, 0, 0, 0);
+  }
+  fill(cell3, i % 2 === 0 ? 0 : 4);
+  // Col 4
+  const cell4 = row.addCell();
+  cell4.formula = line[3];
+  cell4.numFmt = '$#,##0.00';
+  cell4.cellType = 'TypeFormula';
+  cell4.style.align.v = 'center';
+  if (i === 0) {
+    border(cell4, 1, 0, 0, 0);
+  } else if (i === len - 1) {
+    border(cell4, 0, 0, 1, 0);
+  } else {
+    border(cell4, 0, 0, 0, 0);
+  }
+  fill(cell4, i % 2 === 0 ? 0 : 4);
+}
+
+for (let i = 0; i < 4; i++) {
+  sheet.col(i).width = 20;
 }
 
 file
--- test/file.test.js
+++ test/file.test.js
@@ -12,7 +12,7 @@ import Zip from 'jszip';
 Zip.defaults.date = new Date(Date.UTC(2016, 10, 23, 0, 0, 0));
 
 describe('Test: file.js', () => {
-  it('should saveAs buffer', (done) => {
+  it('should saveAs simple excel file', (done) => {
     const file = new File();
     const sheet = file.addSheet('hello');
     const row = sheet.addRow();
@@ -31,8 +31,138 @@ describe('Test: file.js', () => {
 
     cell.style = style;
 
-    const tmpfile = join(tmpdir(), 'file-test.xlsx');
-    const expfile = join(__dirname, 'expect/file-test.xlsx');
+    const tmpfile = join(tmpdir(), 'simple.xlsx');
+    const expfile = join(__dirname, 'expect/simple.xlsx');
+    file
+      .saveAs()
+      .pipe(fs.createWriteStream(tmpfile))
+      .on('finish', () => {
+        const expectFile = fs.createReadStream(expfile);
+        const actualFile = fs.createReadStream(tmpfile);
+        streamEqual(expectFile, actualFile, function (err, ok) {
+          expect(err).to.be.null;
+          expect(ok).to.be.true;
+          done();
+        });
+      });
+  });
+  it('should saveAs complex excel file', (done) => {
+    const file = new File();
+    const sheet = file.addSheet('Sheet1');
+    const data = [
+      ['Auto', 200, 90, 'B2-C2'],
+      ['Entertainment', 200, 32, 'B3-C3'],
+      ['Food', 350, 205.75, 'B4-C4'],
+      ['Home', 300, 250, 'B5-C5'],
+      ['Medical', 100, 35, 'B6-C6'],
+      ['Personal Items', 300, 80, 'B7-C7'],
+      ['Travel', 500, 350, 'B8-C8'],
+      ['Utilities', 200, 100, 'B9-C9'],
+      ['Other', 50, 60, 'B10-C10']
+    ];
+
+    function border (cell, top, left, bottom, right) {
+      const light = 'ffded9d4';
+      const dark = 'ff7e6a54';
+      cell.style.border.top = 'thin';
+      cell.style.border.topColor = top ? dark : light;
+      cell.style.border.left = 'thin';
+      cell.style.border.leftColor = left ? dark : light;
+      cell.style.border.bottom = 'thin';
+      cell.style.border.bottomColor = bottom ? dark : light;
+      cell.style.border.right = 'thin';
+      cell.style.border.rightColor = right ? dark : light;
+    }
+
+    function fill (cell, type) {
+      type = type || 0;
+      const colors = ['ffffffff', 'ffa2917d', 'ffe4e2de', 'fffff8df', 'fff1eeec'];
+      // 1: header, 2: first col, 3: second col, 4: gray, 0: white
+      cell.style.fill.patternType = 'solid';
+      cell.style.fill.fgColor = colors[type];
+      cell.style.fill.bgColor = 'ffffffff';
+    }
+
+    const header = sheet.addRow();
+    header.setHeightCM(0.8);
+    const headers = ['Category', 'Budget', 'Actual', 'Difference'];
+    for (let i = 0; i < headers.length; i++) {
+      const hc = header.addCell();
+      hc.value = headers[i];
+      hc.style.align.v = 'center';
+      if (i > 0) hc.style.align.h = 'right';
+      hc.style.font.color = 'ffffffff';
+      border(hc, 0, 0, 1, 0);
+      fill(hc, 1);
+    }
+
+    const len = data.length;
+    for (let i = 0; i < len; i++) {
+      const line = data[i];
+      const row = sheet.addRow();
+      row.setHeightCM(0.8);
+      // Col 1
+      const cell1 = row.addCell();
+      cell1.value = line[0];
+      cell1.style.align.v = 'center';
+      if (i === 0) {
+        border(cell1, 1, 0, 0, 1);
+      } else if (i === len - 1) {
+        border(cell1, 0, 0, 1, 1);
+      } else {
+        border(cell1, 0, 0, 0, 1);
+      }
+      fill(cell1, 2);
+      // Col 2
+      const cell2 = row.addCell();
+      cell2.value = line[1];
+      cell2.numFmt = '$#,##0.00';
+      cell2.cellType = 'TypeNumeric';
+      cell2.style.align.v = 'center';
+      if (i === 0) {
+        border(cell2, 1, 1, 0, 0);
+      } else if (i === len - 1) {
+        border(cell2, 0, 1, 1, 0);
+      } else {
+        border(cell2, 0, 1, 0, 0);
+      }
+      fill(cell2, 3);
+      // Col 3
+      const cell3 = row.addCell();
+      cell3.value = line[2];
+      cell3.numFmt = '$#,##0.00';
+      cell3.cellType = 'TypeNumeric';
+      cell3.style.align.v = 'center';
+      if (i === 0) {
+        border(cell3, 1, 0, 0, 0);
+      } else if (i === len - 1) {
+        border(cell3, 0, 0, 1, 0);
+      } else {
+        border(cell3, 0, 0, 0, 0);
+      }
+      fill(cell3, i % 2 === 0 ? 0 : 4);
+      // Col 4
+      const cell4 = row.addCell();
+      cell4.formula = line[3];
+      cell4.numFmt = '$#,##0.00';
+      cell4.cellType = 'TypeFormula';
+      cell4.style.align.v = 'center';
+      if (i === 0) {
+        border(cell4, 1, 0, 0, 0);
+      } else if (i === len - 1) {
+        border(cell4, 0, 0, 1, 0);
+      } else {
+        border(cell4, 0, 0, 0, 0);
+      }
+      fill(cell4, i % 2 === 0 ? 0 : 4);
+    }
+
+    for (let i = 0; i < 4; i++) {
+      sheet.col(i).width = 20;
+    }
+
+    const tmpfile = join(tmpdir(), 'complex.xlsx');
+    const expfile = join(__dirname, 'expect/complex.xlsx');
     file
       .saveAs()
       .pipe(fs.createWriteStream(tmpfile))
